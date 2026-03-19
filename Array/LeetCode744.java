e54c1db2067b5f9d4ed18206ab02021ab525e213
Rushi751 <panchalrushikesh2467890@gmail.com>
2026-03-19 13:09:59 +0530

    LeetCode744

---
+++ Array/LeetCode744.java
@@ -0,0 +1,71 @@
+/*
+You are given an array of characters letters that is sorted in non-decreasing order, and a character target. There are at least two different characters in letters.
+
+Return the smallest character in letters that is lexicographically greater than target. If such a character does not exist, return the first character in letters.
+
+
+
+Example 1:
+
+Input: letters = ["c","f","j"], target = "a"
+Output: "c"
+Explanation: The smallest character that is lexicographically greater than 'a' in letters is 'c'.
+Example 2:
+
+Input: letters = ["c","f","j"], target = "c"
+Output: "f"
+Explanation: The smallest character that is lexicographically greater than 'c' in letters is 'f'.
+Example 3:
+
+Input: letters = ["x","x","y","y"], target = "z"
+Output: "x"
+Explanation: There are no characters in letters that is lexicographically greater than 'z' so we return letters[0].
+
+
+Constraints:
+
+2 <= letters.length <= 104
+letters[i] is a lowercase English letter.
+letters is sorted in non-decreasing order.
+letters contains at least two different characters.
+target is a lowercase English letter.
+ */
+
+/*
+Time Complexity: O(n)
+Space Complexity: O(1)
+ */
+package Array;
+
+import java.util.Scanner;
+
+public class LeetCode744 {
+
+    public static char nextGreatestLetter(char[] letters, char target) {
+        for (char letter : letters) {
+            if (letter > target) {
+                return letter;
+            }
+        }
+     return letters[0];
+
+    }
+
+        public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array");
+        int n = sc.nextInt();
+        System.out.println("Enter the target character");
+        char target = sc.next().charAt(0);
+
+        char[] arr = new char[n];
+        System.out.println("Enter the elements of the array");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.next().charAt(0);
+        }
+
+        System.out.println("Next greatest element is "+nextGreatestLetter(arr,target));
+        sc.close();
+
+    }
+}
